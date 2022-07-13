cb1e7c3296c4c893175afef5d51853b3080aed8f
Ali Momen <98864491+alimomen10@users.noreply.github.com>
2022-07-13 14:47:07 -0400

    Update index.js

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -14,7 +14,7 @@ function Projects() {
                     <img
                       className="d-block w-100"
                       src="https://images.unsplash.com/photo-1473186505569-9c61870c11f9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
-                      alt="First slide"
+                      alt="a letter with a pen on top"
                     />
                 </a>
                 <Carousel.Caption>
