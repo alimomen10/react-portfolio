233aa3fe3086d603121e923e207f6b90d3e7ed38
Ali Momen <98864491+alimomen10@users.noreply.github.com>
2022-07-13 12:30:56 -0400

    Finding a new path

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 
 function App() {
   return (
-  <h2>Let's get to work</h2>
-  );
+    <div>
+     <h1>HELLO!</h1>
+    </div>
+  )
+ 
 }
 
 export default App;
