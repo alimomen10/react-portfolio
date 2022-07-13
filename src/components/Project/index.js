8df990c9bf9c0c33fa1a416a820c60df681d7d53
Ali Momen <98864491+alimomen10@users.noreply.github.com>
2022-07-13 14:47:52 -0400

    Update index.js

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -23,11 +23,13 @@ function Projects() {
                 </Carousel.Caption>
               </Carousel.Item>
               <Carousel.Item>
-                <img
-                  className="d-block w-100"
-                  src="https://images.unsplash.com/photo-1504616267454-5460d659c9be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
-                  alt="Woman standing in the mist with a raincoat on"
-                />
+                <a href="https://wearther-a-squad.github.io/interactive-proj-wearther/">
+                    <img
+                      className="d-block w-100"
+                      src="https://images.unsplash.com/photo-1504616267454-5460d659c9be?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80"
+                      alt="Woman standing in the mist with a raincoat on"
+                    />
+                </a>
                 <Carousel.Caption>
                   <h3>Wearther</h3>
                   <p>Understand the weather by knowing exactly what to wear</p>
